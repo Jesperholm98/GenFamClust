531adfe153ce67fbb31d1945fec73a17b0c47466
Jesperholm98 <jesperholm98@hotmail.com>
2021-10-21 16:37:05 +0200

    added basic code to main file to take two input filenames

--- GenFamClust/main.py
+++ GenFamClust/main.py
@@ -1,1 +1,15 @@
-asdf
+import sys
+
+def main():
+    args = sys.argv[1:]
+
+    if len(args) != 2:
+        print("programe only takes two input filenames!")
+        return 1
+    else:
+        print("First filename: " + args[0])
+        print("Second filename: " + args[1])
+
+
+if __name__ == "__main__":
+    main()
