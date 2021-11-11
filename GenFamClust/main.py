a9d9d304e51c6b98ca975567edb7052c846b4ec2
Jesperholm98 <jesperholm98@hotmail.com>
2021-11-11 19:00:59 +0100

    restructured folders a bit

---
+++ GenFamClust/main.py
@@ -0,0 +1,17 @@
+import sys
+import numpy as np
+
+
+def main():
+    args = sys.argv[1:]
+
+    if len(args) != 2:
+        print("programe only takes two input filenames!")
+        return 1
+    else:
+        print("First filename: " + args[0])
+        print("Second filename: " + args[1])
+
+
+if __name__ == "__main__":
+    main()
