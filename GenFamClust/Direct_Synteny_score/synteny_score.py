771c30f4c651b2e5d2dd99910ca9fa39e26bd980
Jesperholm98 <jesperholm98@hotmail.com>
2021-11-15 23:18:10 +0100

    Changed some code to now read files with the correct format, and while reading synteny data remove lines where the no identifier exist. Also Changed so that NC_scores now get saved into a dictionary rather than a numpy array.(loading NC-data into numpy array took around 30 secs while loading it into a dict took about 6 seconds.)

--- GenFamClust/Direct_Synteny_score/synteny_score.py
+++ GenFamClust/Direct_Synteny_score/synteny_score.py
@@ -1,35 +1,97 @@
 import sys
 import numpy as np
 import time
-
+import multiprocessing as mp
 
 
 def main(argv):
     synteny_score()
 
-def synteny_score(NC_scores='nc.txt', querySyntenyFile='test.txt'):
+
+def synteny_score(NC_scores='nc.txt', querySyntenyFile1='human_locs.txt', querySyntenyFile2='mouse_locs.txt'):
 
     #querySyntenyfile
-    gene_order = np.loadtxt(querySyntenyFile, dtype={'names': ('contig', 'gene'), 'formats': ('U8', 'U6')})
+    #gene_order = np.loadtxt(querySyntenyFile1, dtype={'names': ('contig', 'gene'), 'formats': ('U8', 'U6')})
+
+    Synteny1 = []
+    wrongF = 0
+    totalF = 0
+
+    #load SyntenyFile1
+    with open(querySyntenyFile1) as file:
+        for lines in file:
+            lines = lines.strip().split()
+            totalF += 1
+            if len(lines) == 3:
+                #only keep the lines where there exist values in all three columns
+                Synteny1.append(lines)
+            else:
+                wrongF += 1
+
+    print("SyntenyFile1 finished loading.")
+    print("Total nr of lines in First Synteny file: ", totalF)
+    print("Nr of bad lines in First Synteny file: ", wrongF)
+    print("Synteny1 has: ", len(Synteny1), " elements.\n")
+
+
+    Synteny2 = []
+
+    wrongS = 0
+    totalS = 0
+
+    #load SyntenyFile2
+    with open(querySyntenyFile2) as file:
+        for lines in file:
+            lines = lines.strip().split()
+            totalS += 1
+            if len(lines) == 3:
+                # only keep the lines where there exist values in all three columns
+                Synteny2.append(lines)
+            else:
+                wrongS += 1
+
+    print("SyntenyFile2 finished loading.")
+    print("Total nr of lines in Second Synteny file: ", totalS)
+    print("Nr of bad lines in Second Synteny file: ", wrongS)
+    print("Synteny2 has: ", len(Synteny2), " elements.\n")
+
+
 
     #load NC file into either nparrray OR a dict.
     print("start loading NC_pairs...")
-    t = time.process_time()
-    NC_pairs = np.loadtxt(NC_scores, dtype={'names': ('gene1', 'gene2', 'NC_score'), 'formats': ('U6', 'U6', 'f8')})
-    end = time.time()
+    t1 = time.process_time()
+    dict = {}
+    #NC_pairs = np.loadtxt(NC_scores, dtype={'names': ('gene1', 'gene2', 'NC_score'), 'formats': ('U6', 'U6', 'f8')})
+
+    inDict = 0
+    total = 0
+    with open(NC_scores) as file:
+        for lines in file:
+            lines = lines.strip().split()
+            total += 1
+            #if (lines[0], lines[1]) in dict or (lines[1], lines[0]) in dict:
+            #    #if either (A, B) OR (B, A) already exist in dict no need to try and adding it, so skip if-statement below.
+            #    continue
+            if lines[0] != lines[1]:
+                dict[(lines[0], lines[1])] = lines[2]
+                inDict += 1
+
     print("finished loading NC_pairs...")
-    elapsed_time = time.process_time() - t
+    elapsed_time = time.process_time() - t1
     print("seconds to load NC-data: ", elapsed_time)
+    print("Total elements in file: ", total)
+    print("Total elements in DICT: ", inDict)
 
-    one = NC_pairs[1]
-    gene1 = one[0]
-    gene2 = one[1]
-    NC_Value = one[2]
 
-    print("One line: ", one)
-    print("type of gene1: ", type(gene1))
-    print("type of gene2: ", type(gene2))
-    print("type of NC_Value: ", type(NC_Value))
+    #one = NC_pairs[1]
+    #gene1 = one[0]
+    #gene2 = one[1]
+    #NC_Value = one[2]
+
+    #print("One line: ", one)
+    #print("type of gene1: ", type(gene1))
+    #print("type of gene2: ", type(gene2))
+    #print("type of NC_Value: ", type(NC_Value))
 
     # calculation
 
