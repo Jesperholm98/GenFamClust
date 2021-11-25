1c1da6ec4e2cdcf74b7f39d1cee259ecf40b217d
Jesperholm98 <jesperholm98@hotmail.com>
2021-11-25 22:53:47 +0100

    Added function to infer homology between genes from nc_values and syc_values

--- GenFamClust/Direct_Synteny_score/synteny_score.py
+++ GenFamClust/Direct_Synteny_score/synteny_score.py
@@ -408,6 +408,23 @@ def synteny_score(NC_scores='nc.txt', querySyntenyFile1='human_locs.txt', queryS
     print("Total size of help_dict: ", sys.getsizeof(help_dict))
     #print("#mis pairs not in SyS:", len(not_found_keys))
 
+def infer_homology(syc_values, nc_values):
+    #load nc_values into memory
+    nc_dict = {}
+    with open(nc_values, 'r') as file:
+        for lines in file:
+            lines = lines.split()
+            nc_dict[(lines[0], lines[1])] = float(lines[2])
+
+    f = open('homolog_genes.txt', 'a')
+    print("Starting to infer homology")
+    with open(syc_values, 'r'):
+        for lines in file:
+            lines = lines.split()
+            if (nc_dict[(lines[0], lines[1])]**2 + 0.25 * float(lines[2])**2 - 0.25) > 0:
+                f.write(lines[0] + "\t" + lines[1] + "\n")
+    f.close()
+    print("Done.")
 
 
 
